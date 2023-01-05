7bdb3f6a93fe1836d89e0fd1e45c2c2699db4f42
anubhav666 <meliodasanubhavdutta@gmail.com>
2023-01-05 13:06:21 +0530

    Updated API

--- news-api-main.py
+++ news-api-main.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import json
 import requests
-from newspaper import Article
 from transformers import pipeline
 
 
@@ -20,21 +18,5 @@ def run():
     }
     response = requests.request("GET", url, headers=headers, params=querystring)
     print(response)
-    # response_dict = json.loads(response.text)
-    # links = [response_dict['articles'][i]['link'] for i in range(len(response_dict['articles']))]
-    # for link in links:
-    #     news_article = Article(link, language = 'en')
-    #     news_article.download()
-    #     news_article.parse()
-    #     article_titles.append(news_article.title)
-    #     article_texts.append(news_article.text)
-    # for text in article_texts:
-    #     article_summaries.append(summarizer(text)[0]['summary_text'])
-    # for i in range(len(article_texts)):
-    #     st.header(article_titles[i])
-    #     st.subheader('Summary of Article')
-    #     st.markdown(article_summaries[i])
-    #     with st.expander('Full Article'):
-    #         st.markdown(article_texts[i])
 if __name__=='__main__':
     run()
